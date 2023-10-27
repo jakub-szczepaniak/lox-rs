f4fb4d856a580d5cbd421afc1a8ddb9e075f3a98
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2023-10-27 21:50:57 +0200

    feat: Creates scaffold for token scanner.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::env::args;
-use std::io::{Read, BufReader};
-use std::fs::File;
+use std::io::{BufRead,  self};
 
 fn main() {
     let args: Vec<String> = args().collect();
     println!("Hello, world! {:?}", args);
-    println!("Hello, world! {:?}", args.len());
+    
     if args.len() > 1 {
         println!("Usage: lox-rs [script]");
         std::process::exit(64);
@@ -18,18 +17,52 @@ fn main() {
     println!("Hello, world! {:?}", args);
 }
 
-fn run_file(path: &str) {
-    let file = File::open(path).expect("file not found");
-    let mut buf_reader = BufReader::new(file); 
-    let mut buf = Vec::<u8>::new();
-
-    buf_reader.read_to_end(&mut buf).expect("error reading file");
-
-    run(&buf);
-
+fn run_file(path: &str) -> io::Result<()> {
+    let contents = match std::fs::read_to_string(path) {
+        Ok(contents) => contents,
+        Err(e) => {
+            println!("Error reading file: {}", e);
+            return Ok(());
+        }
+    };
+    
+    
+    run(&contents);
+    Ok(())
 }
-fn run_prompt() {}
+fn run_prompt() {
+    let stdin = io::stdin();
+    print!("> ");
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        run(&line);
+        print!("> ");
+    }
+}
 
-fn run(source: &[u8]) {
-    println!("run: {:?}", source);
+fn run(source: &str) {
+    let mut scanner = Scanner::new(source);
+    let tokens = scanner.scan_tokens();
+    for token in tokens {
+        println!("{:?}", token);
+    }
+}
+
+#[derive(Debug)]
+struct Token {
+    token: String,
+}
+
+struct Scanner{
+    source: String,
+}
+impl Scanner {
+    fn new (source: &str) -> Scanner {
+        Scanner {
+            source: source.to_string(),
+        }
+    }
+    fn scan_tokens(self) -> Vec<Token> {
+        Vec::<Token>::new()
+    }
 }
