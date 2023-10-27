ac7c6f9b8353664d9c5e188e869649420932fe9c
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2023-10-27 17:10:31 +0200

    feat: Handle input from user.

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::env::args;
+use std::io::{Read, BufReader};
+use std::fs::File;
+
+fn main() {
+    let args: Vec<String> = args().collect();
+    println!("Hello, world! {:?}", args);
+    
+    if args.len() > 1 {
+        println!("Usage: lox-rs [script]");
+        std::process::exit(64);
+    } else if args.len() == 1 {
+        run_prompt();
+    } else {
+        run_file(&args[1]);
+    }
+
+    println!("Hello, world! {:?}", args);
+}
+
+fn run_file(path: &str) {
+    let file = File::open("input.txt").expect("file not found");
+    let mut buf_reader = BufReader::new(file); 
+    let mut buf = Vec::<u8>::new();
+
+    buf_reader.read_to_end(&mut buf).expect("error reading file");
+    
+
+}
+fn run_prompt() {}
+
