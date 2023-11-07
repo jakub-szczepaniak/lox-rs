d5b2d5922f98a53fc2dc317d0bfc6133b4d8602b
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2023-11-07 23:30:48 +0100

    fix: Fixes Clippy warnings.

--- lox-gen-ast/src/main.rs
+++ lox-gen-ast/src/main.rs
@@ -26,19 +26,19 @@ fn define_ast(output_dir: &str, base_name: &str, types: &[&str]) -> io::Result<(
     let path = format!("{}/{}.rs", output_dir, base_name.to_lowercase());
     let mut file = std::fs::File::create(path).unwrap();
     let mut tree_types = Vec::<TreeType>::new();
+    
     writeln!(file, "use crate::error::*;")?;
+    writeln!(file, "use crate::token::*;")?;
 
-    write!(file, "use crate::token::*;\n")?;
 
     for ttype in types {
-        let (base_class, fields) = ttype.split_once(":").unwrap();
-        println!("base_class: {}, fields: {}", base_class, fields);
+        let (base_class, fields) = ttype.split_once(':').unwrap();
         let class_name = format!("{}{}", base_name.trim(), base_class);
-        println!("class_name: {}", class_name);
-        let args  = fields.split(",");
+        let args  = fields.split(',');
         let mut field_names = Vec::new();
+        
         for arg in args {
-            let (field_type, name) = arg.trim().split_once(" ").unwrap();
+            let (field_type, name) = arg.trim().split_once(' ').unwrap();
             field_names.push(format!("{}: {},\n", name, field_type));
         }
         tree_types.push(TreeType { 
@@ -46,27 +46,27 @@ fn define_ast(output_dir: &str, base_name: &str, types: &[&str]) -> io::Result<(
             class_name: class_name.trim().to_string(), 
             fields: field_names})
     }
-    write!(file,"\n pub enum {base_name} {{ \n")?;
+    writeln!(file,"\n pub enum {base_name} {{")?;
     for t in &tree_types {
-        write!(file,"    {}({}),\n", t.base_class, t.class_name)?;
+        writeln!(file,"    {}({}),", t.base_class, t.class_name)?;
     }
-    write!(file,"}}\n\n")?;
+    writeln!(file,"}}\n")?;
     
     for t in &tree_types {
-        write!(file, "pub struct {} {{\n", t.class_name)?;
+        writeln!(file, "pub struct {} {{", t.class_name)?;
         for field in &t.fields {
             write!(file, "    {}", field)?;
         }
-        write!(file,"}}\n\n")?;
+        writeln!(file,"}}\n")?;
     }
 
     writeln!(file, "pub trait ExprVisitor<T> {{")?;
 
     for t in &tree_types {
-        writeln!(file, "    fn visit_{}_{}(&self, expr: &{}) -> Result<T, LoxError> {{}}",
-    t.base_class.to_lowercase(),
-    base_name.to_lowercase(),
-    t.class_name)?;
+        writeln!(file, "    fn visit_{}_{}(&self, expr: &{}) -> Result<T, LoxError> ;",
+            t.base_class.to_lowercase(),
+            base_name.to_lowercase(),
+            t.class_name)?;
 
     }
 
@@ -74,7 +74,7 @@ fn define_ast(output_dir: &str, base_name: &str, types: &[&str]) -> io::Result<(
 
     for t in &tree_types {
         writeln!(file, "impl {} {{", t.class_name)?;
-        writeln!(file, "    pub fn accept<T>(&self, visitor: &dyn {}Visitor<T>) -> Result<T, LoxError> {{", base_name);
+        writeln!(file, "    pub fn accept<T>(&self, visitor: &dyn {}Visitor<T>) -> Result<T, LoxError> {{", base_name)?;
         writeln!(file, "        visitor.visit_{}_{}(self)", t.base_class.to_lowercase(), base_name.to_lowercase())?;
         writeln!(file, "    }}")?;
         writeln!(file, "}}\n")?;
