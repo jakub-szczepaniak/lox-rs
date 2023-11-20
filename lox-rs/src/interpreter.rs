69aa781ff53563e3eda216f18ec9929ea9ee9f2e
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2023-11-21 00:13:28 +0100

    feat: Implemented division operator.

--- lox-rs/src/interpreter.rs
+++ lox-rs/src/interpreter.rs
@@ -6,21 +6,30 @@ use crate::token_type::*;
 
 pub struct Interpreter {}
 
-
 impl ExprVisitor<Literal> for Interpreter {
     fn visit_binary_expr(&self, expr: &ExprBinary) -> Result<Literal, LoxError> {
         let left = self.evaluate(&expr.left)?;
         let right = self.evaluate(&expr.right)?;
 
         match expr.operator.token_type() {
-            TokenType::Minus => {
-                match (left, right) {
-                    (Literal::Number(x),Literal::Number(y)) => Ok(Literal::Number(x-y)),
-                    _ => { todo!("not yet implemented")}
+            TokenType::Minus => match (left, right) {
+                (Literal::Number(x), Literal::Number(y)) => Ok(Literal::Number(x - y)),
+                _ => {
+                    todo!("not yet implemented")
                 }
             },
-            _ => { todo!("not implemented")} 
-
+            TokenType::Slash => match (left, right) {
+                (Literal::Number(_), Literal::Number(0.0)) => {
+                    Err(LoxError::error(0, "Cannot divide by 0"))
+                }
+                (Literal::Number(x), Literal::Number(y)) => Ok(Literal::Number(x / y)),
+                _ => {
+                    todo!("Not implemented")
+                }
+            },
+            _ => {
+                todo!("not implemented")
+            }
         }
     }
     fn visit_grouping_expr(&self, expr: &ExprGrouping) -> Result<Literal, LoxError> {
@@ -66,25 +75,27 @@ impl Interpreter {
 #[cfg(test)]
 mod tests {
 
-    use std::{result, io::LineWriter};
+    use std::{io::LineWriter, result};
 
     use super::*;
 
     fn make_literal(literal: Literal) -> Box<Expr> {
-        Box::new(Expr::Literal(ExprLiteral { value: Some(literal) }))
+        Box::new(Expr::Literal(ExprLiteral {
+            value: Some(literal),
+        }))
     }
-    
+
     fn make_token_operator(ttype: TokenType, lexeme: &str) -> Token {
         Token::new(ttype, lexeme.to_string(), 0, None)
     }
-
 
     #[test]
     fn test_the_unary_minus_for_number() {
         let interp = Interpreter {};
         let unary_minus = Expr::Unary(ExprUnary {
             operator: make_token_operator(TokenType::Minus, "-"),
-            right: make_literal(Literal::Number(42.0)),});
+            right: make_literal(Literal::Number(42.0)),
+        });
         let result = interp.evaluate(&unary_minus);
         assert!(result.is_ok());
         assert_eq!(result.ok(), Some(Literal::Number(-42.0)))
@@ -94,60 +105,91 @@ mod tests {
     fn test_the_unary_minus_for_string() {
         let interp = Interpreter {};
         let unary_minus_string = Expr::Unary(ExprUnary {
-             operator: make_token_operator(TokenType::Minus, "."), 
-             right: make_literal(Literal::String("some".to_string()))});
-        
+            operator: make_token_operator(TokenType::Minus, "."),
+            right: make_literal(Literal::String("some".to_string())),
+        });
+
         let result = interp.evaluate(&unary_minus_string);
-       assert!(result.is_ok());
-       assert_eq!(result.ok(), Some(Literal::Nil) );
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Literal::Nil));
     }
-    
+
     #[test]
     fn test_unary_bang_true_result_false() {
         let interp = Interpreter {};
         let unary_bang_true = Expr::Unary(ExprUnary {
-             operator: make_token_operator(TokenType::Bang, "!"), 
-             right:  make_literal(Literal::Boolean(true))}) ;
+            operator: make_token_operator(TokenType::Bang, "!"),
+            right: make_literal(Literal::Boolean(true)),
+        });
         let result = interp.evaluate(&unary_bang_true);
         assert!(result.is_ok());
         assert_eq!(result.ok(), Some(Literal::Boolean(false)));
     }
-    
+
     #[test]
     fn test_unary_bang_false_result_true() {
         let interp = Interpreter {};
         let unary_bang_false = Expr::Unary(ExprUnary {
             operator: make_token_operator(TokenType::Bang, "!"),
-            right: make_literal(Literal::Boolean(false)) });
-  
+            right: make_literal(Literal::Boolean(false)),
+        });
+
         let result = interp.evaluate(&unary_bang_false);
 
         assert!(result.is_ok());
         assert_eq!(result.ok(), Some(Literal::Boolean(true)));
-   }
+    }
 
-   #[test]
-  fn test_unary_bang_nil_result_true() {
-        let interp = Interpreter {};    
+    #[test]
+    fn test_unary_bang_nil_result_true() {
+        let interp = Interpreter {};
         let unary_bang_nil = Expr::Unary(ExprUnary {
             operator: make_token_operator(TokenType::Bang, "!"),
-            right: make_literal(Literal::Nil) });
-   
+            right: make_literal(Literal::Nil),
+        });
+
         let result = interp.evaluate(&unary_bang_nil);
 
         assert!(result.is_ok());
         assert_eq!(result.ok(), Some(Literal::Boolean(true)));
-   }
+    }
 
-   #[test]
-   fn test_binary_substraction() {
-    let interp = Interpreter {};
-    let binary_minus = Expr::Binary(ExprBinary { 
-        left: make_literal(Literal::Number(13.0)), 
-        operator: make_token_operator(TokenType::Minus, "-"), 
-        right: make_literal(Literal::Number(12.0)) });
-    let result = interp.evaluate(&binary_minus);
-    assert!(result.is_ok());
-    assert_eq!(result.ok(), Some(Literal::Number(1.0)));
-   }
+    #[test]
+    fn test_binary_substraction() {
+        let interp = Interpreter {};
+        let binary_minus = Expr::Binary(ExprBinary {
+            left: make_literal(Literal::Number(13.0)),
+            operator: make_token_operator(TokenType::Minus, "-"),
+            right: make_literal(Literal::Number(14.0)),
+        });
+        let result = interp.evaluate(&binary_minus);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Literal::Number(-1.0)));
+    }
+
+    #[test]
+    fn test_binary_division() {
+        let interp = Interpreter {};
+        let binary_slash = Expr::Binary(ExprBinary {
+            left: make_literal(Literal::Number(10.0)),
+            operator: make_token_operator(TokenType::Slash, "/"),
+            right: make_literal(Literal::Number(2.0)),
+        });
+
+        let result = interp.evaluate(&binary_slash);
+        assert!(result.is_ok());
+        assert_eq!(result.ok(), Some(Literal::Number(5.0)))
+    }
+    #[test]
+    fn test_binary_division_by_zero() {
+        let interp = Interpreter {};
+        let binary_slash_by_zero = Expr::Binary(ExprBinary {
+            left: make_literal(Literal::Number(42.0)),
+            operator: make_token_operator(TokenType::Slash, "/"),
+            right: make_literal(Literal::Number(0.0)),
+        });
+
+        let result = interp.evaluate(&binary_slash_by_zero);
+        assert!(result.is_err());
+    }
 }
