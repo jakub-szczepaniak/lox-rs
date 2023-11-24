b66d8730e52366a804611c24ff0ffbaa23bf5023
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2023-11-24 23:49:01 +0100

    refactoring: Separated interpreter code from the main code.

--- lox-rs/src/main.rs
+++ lox-rs/src/main.rs
@@ -4,7 +4,6 @@ mod parser;
 mod scanner;
 mod token;
 mod token_type;
-use ast_print::AstPrinter;
 use error::LoxError;
 use scanner::Scanner;
 use std::env::args;
@@ -17,10 +16,10 @@ mod literal;
 
 fn main() {
     let args: Vec<String> = args().collect();
-
+    let lox = Lox::new();
     match args.len() {
-        1 => run_prompt(),
-        2 => run_file(&args[1]),
+        1 => lox.run_prompt(),
+        2 => lox.run_file(&args[1]),
         _ => {
             println!("Usage: lox-rs [script]");
             std::process::exit(64);
@@ -28,61 +27,72 @@ fn main() {
     }
 }
 
-fn run_file(path: &str) {
-    let contents = match std::fs::read_to_string(path) {
-        Ok(contents) => contents,
-        Err(e) => {
-            println!("Error reading file: {}", e);
-            std::process::exit(64);
-        }
-    };
+struct Lox {
+    interpreter: Interpreter,
+}
 
-    match run(contents) {
-        Ok(_) => (),
-        Err(_e) => {
-            std::process::exit(65);
+impl Lox {
+
+    pub fn new() -> Lox {
+        Lox {
+            interpreter  : Interpreter {}
         }
     }
-}
-fn run_prompt() {
-    print!("> ");
-    let _ = stdout().flush();
+    pub fn run_file(&self, path: &str) {
+        let contents = match std::fs::read_to_string(path) {
+            Ok(contents) => contents,
+            Err(e) => {
+                println!("Error reading file: {}", e);
+                std::process::exit(64);
+            }
+        };
 
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        if let Ok(line) = line {
-            if line.is_empty() {
-                continue;
+            match self.run( contents) {
+            Ok(_) => (),
+            Err(_e) => {
+                std::process::exit(65);
             }
-            match run(line) {
-                Ok(_) => (),
-                Err(_e) => {
-                    // error was already reported in scanner
-                }
-            }
-        } else {
-            break;
         }
+    }
+    pub fn run_prompt(&self) {
         print!("> ");
         let _ = stdout().flush();
-    }
-}
 
-fn run(source: String) -> Result<(), LoxError> {
-    let mut scanner = Scanner::new(source);
-    let tokens = scanner.scan_tokens()?;
-    let mut parser = parser::Parser::new(tokens.clone());
-
-    match parser.parse() {
-        None => Err(LoxError {
-            token: None,
-            message: "Error when parsing".to_string(),
-            line: 0,
-        }),
-        Some(expr) => {
-            let interpreter = Interpreter {};
-            interpreter.interprete(&expr);
-            Ok(())
+        let stdin = io::stdin();
+        for line in stdin.lock().lines() {
+            if let Ok(line) = line {
+                if line.is_empty() {
+                    continue;
+                }
+                match self.run(line) {
+                    Ok(_) => (),
+                    Err(_e) => {
+                        // error was already reported in scanner
+                    }
+                }
+            } else {
+                break;
+            }
+            print!("> ");
+            let _ = stdout().flush();
         }
     }
+    fn run(&self, source: String) -> Result<(), LoxError> {
+        let mut scanner = Scanner::new(source);
+        let tokens = scanner.scan_tokens()?;
+        let mut parser = parser::Parser::new(tokens.clone());
+
+        match parser.parse() {
+            None => Err(LoxError {
+                token: None,
+                message: "Error when parsing".to_string(),
+                line: 0,
+            }),
+            Some(expr) => {
+                self.interpreter.interprete(&expr);
+                Ok(())
+            }
+        }
+    }
+
 }
